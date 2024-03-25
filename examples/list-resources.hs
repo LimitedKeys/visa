ce4270a4263b800c8fe91499742639b862daea39
Richard Hoberecht <richard.hoberecht@infineon.com>
2024-03-25 15:45:05 -0700

    Implement 'find' function to make finding resources much easier

--- examples/list-resources.hs
+++ examples/list-resources.hs
@@ -2,27 +2,18 @@ module Main where
 
 import Visa.Resources
 
-import Control.Monad
-
-find_and_print fs _ = do
-    desc <- findNext fs
-    putStrLn ("  " ++ desc)
-
-find_more find_session remaining = forM_ [0..remaining] (find_and_print find_session)
+import Control.Monad (mapM_)
 
 main :: IO ()
 main = do
     session <- defaultSession
-    (find_session, description, connected) <- findResource session "?*::INSTR"
 
-    if connected < 1
-    then putStrLn "No Devices Connected"
-    else do
+    devices <- find session "?*::INSTR"
+    
+    if (length devices) > 0
+    then do
         putStrLn "Devices"
-        putStrLn ("  " ++ description)
-        if connected > 1
-        then find_more find_session (connected - 1)
-        else pure ()
+        mapM_ (\a -> putStrLn (" " ++ a)) devices
+    else pure ()
 
-    close find_session
     close session
--- src/Visa/Resources.hs
+++ src/Visa/Resources.hs
@@ -1,16 +1,18 @@
 module Visa.Resources (defaultSession
                       ,close
-                      ,findResource
-                      ,findNext
+                      ,find
                       ) where
 
 import Foreign
 import Foreign.C.Types
 import Foreign.C.String
 
--- Local imports
+import Data.Foldable (foldrM)
+
 import Visa.Dll
 import Visa.Status
+
+-- TODO withSession + bracket
 
 defaultSession :: IO (ViSession)
 defaultSession = alloca (\session -> do
@@ -23,6 +25,32 @@ close obj = do
     error <- viClose obj 
     check error () "viClose"
 
+-- Find all attached devices based on the query, and return a list
+-- 
+-- Args:
+--   session -> From `defaultSession`
+--   query -> Query String. REGEX Like. A good default is "?*::INSTR"
+--
+-- Returns:
+--   List of connected devices (as a list of strings)
+find :: ViSession -> String -> IO [String]
+find session query = do
+    (find_session, description, connected) <- findResource session query
+    if connected < 1
+    then do
+        close find_session
+        return []
+    else do
+        if connected == 1
+        then do 
+            close find_session
+            return [description]
+        else do -- > 1
+            others <- _find find_session (connected - 1)
+            close find_session
+            return ([description] ++ others)
+
+-- Internal Only
 findResource :: ViSession -> String -> IO (ViFindList, String, Integer)
 findResource session query = withCString query (\c_query -> 
     allocaBytes 256 (\description -> 
@@ -40,7 +68,10 @@ findNext find_session = allocaBytes 256 (\description -> do
     error <- viFindNext find_session description
     desc <- peekCString description
     check error desc "viFindNext")
+            
+-- Find the remainind devices using `findNext`
+_find fs r = foldrM x [] [0..r]
+    where x _ a = do
+            desc <- findNext fs
+            return $ (desc:a)
 
--- Find all attached devices based on the query, and return a list
--- find :: ViSession -> String -> IO ([String])
--- find session query = do
